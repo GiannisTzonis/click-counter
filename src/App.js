cec01f2f4a6c2e63781202e23f8e0d2d1d7b29a6
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-17 06:12:13 +0300

    combined state and props practise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from "react"
+import React from "react";
+import Count from "./Count";
 
 export default function App() {
-    const [count, setCount] = React.useState(0)
-    
-    function add() {
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function subtract() {
-        setCount(prevCount => prevCount - 1)
-    }
-    
-    return (
-        <div className="counter">
-            <button className="counter--minus" onClick={subtract}>–</button>
-            <div className="counter--count">
-                <h1>{count}</h1>
-            </div>
-            <button className="counter--plus" onClick={add}>+</button>
-        </div>
-    )
+  const [count, setCount] = React.useState(0);
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  console.log("App component rendered");
+
+  return (
+    <div className="counter">
+      <button className="counter--minus" onClick={subtract}>
+        –
+      </button>
+      <Count number={count} />
+      <button className="counter--plus" onClick={add}>
+        +
+      </button>
+    </div>
+  );
 }
