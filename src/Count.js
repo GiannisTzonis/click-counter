df083f18feb9cec77e19b6b384baf40a188ca9a8
GiannisTzonis <tzonis.john@hotmail.com>
2022-11-27 13:23:38 +0200

    cleared code

--- src/Count.js
+++ src/Count.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 export default function Count(props) {
-  console.log("Count component rendered");
-
   return (
     <div className="counter--count">
       <h1>{props.number}</h1>
